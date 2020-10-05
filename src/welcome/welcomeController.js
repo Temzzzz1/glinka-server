5e1d0270038a494ac61181ecf9b974d398a89a1b
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-05 18:35:36 +0600

    upd

--- src/welcome/welcomeController.js
+++ src/welcome/welcomeController.js
@@ -1,17 +1,7 @@
 
-const ig = require('instagram-scraping')
 
-module.exports.showInstagramPosts = (req, res) => {
+// module.exports.showInstagramPosts = (req, res) => {
 
-    ig.scrapeUserPage('rusanov.tema').then(result => {
-        
-        postsURL = []
-        for (let index = 0; index < 5; index++) {
-            postsURL.push(result.medias[index]['thumbnail'])
-        }
-        res.json(result)
-
-      });
-
+    
    
-}
+// }
--- src/welcome/welcomeRoute.js
+++ src/welcome/welcomeRoute.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = Router();
 const welcomeController = require('./welcomeController')
 
-router.get('/', welcomeController.showInstagramPosts)
+//router.get('/', )
 
 router.get('/text', (req, res) => {
     res.json({name: 'tema'})
