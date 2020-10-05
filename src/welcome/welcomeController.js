f1eadaca134c1f9aa80ae3446458b35d4a0cf445
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-05 17:57:34 +0600

    Update welcomeController.js

--- src/welcome/welcomeController.js
+++ src/welcome/welcomeController.js
@@ -3,15 +3,19 @@ const ig = require('instagram-scraping')
 
 module.exports.showInstagramPosts = (req, res) => {
 
-    ig.scrapeUserPage('rusanov.tema').then(result => {
-
-        res.json(result)
-        // postsURL = []
-        // for (let index = 0; index < 5; index++) {
-        //     postsURL.push(result.medias[index]['thumbnail'])
-        // }
-        
+    ig.scrapeTag('veranda').then(result => {
+        res.json(result);
       });
+
+    // ig.scrapeUserPage('rusanov.tema').then(result => {
+
+    //     res.json(result)
+    //     // postsURL = []
+    //     // for (let index = 0; index < 5; index++) {
+    //     //     postsURL.push(result.medias[index]['thumbnail'])
+    //     // }
+        
+    //   });
 
    
 }
