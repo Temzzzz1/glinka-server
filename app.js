5197f574fabb2d2de674c3b7c6c1881c61237e15
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-05 11:26:51 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 6080
 
 const app = express();
 
