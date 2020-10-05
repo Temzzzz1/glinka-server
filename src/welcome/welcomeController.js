7cd556daa3b168d9ebbe4d34b684af7f4f702f78
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-05 14:26:08 +0600

    Update welcomeController.js

--- src/welcome/welcomeController.js
+++ src/welcome/welcomeController.js
@@ -4,12 +4,13 @@ const ig = require('instagram-scraping')
 module.exports.showInstagramPosts = (req, res) => {
 
     postsURL = []
-    postsURL = ig.scrapeUserPage('aee.glinka').then(result => {
+    ig.scrapeUserPage('aee.glinka').then(result => {
         
         for (let index = 0; index < 5; index++) {
             postsURL.push(result.medias[index]['thumbnail'])
         }
+        res.json(postsURL)
       });
 
-    res.json(postsURL)
+   
 }
