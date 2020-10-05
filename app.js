27c7b7710f44dbbd7a2b49701a919ebf71b6c7c1
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-05 13:02:39 +0600

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 app.use(cookieParser())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:6080");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Routes
 const adminRoute = require('./src/admin/adminRoute')
 const postRoute = require('./src/posts/postRoute')
