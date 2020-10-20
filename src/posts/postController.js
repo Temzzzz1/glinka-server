8f80a7370be24a4a2f3bbffb939a9b6716a36831
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-20 12:44:33 +0600

    add cloudinary upload controller

--- src/posts/postController.js
+++ src/posts/postController.js
@@ -1,11 +1,11 @@
 const Posts = require('./postModel')
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.CLOUD_API_KEY, 
-    api_secret: process.env.CLOUD_API_SECRET 
-  });
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
 
 module.exports.showPosts = async (req, res) => {
     res.json(await Posts.find({}).lean())
@@ -15,29 +15,18 @@ module.exports.showPosts = async (req, res) => {
 
 module.exports.addPost = async (req, res) => {
 
-    if (req.image) {
-        cloudinary.uploader.upload(req.image, {
+    if (req.files.image) {
+        cloudinary.uploader.upload(req.files.image.path, {
+            'folder': 'glinka/portfolio',
             'crop': 'limit',
             'tags': 'samples',
             'width': 3000,
             'height': 2000
-        }, (result) => console.log(result) );
-    }
-
-    if (req.secureToken == process.env.SECURE_TOKEN) {
-        const post = new Posts({ 
-            title: req.title,
-            imageUrl: req.imageUrl
-         });
-     
-        
-        post.save(function (err) {
-          if (err) return res.json({isError: err})
+        }, (error, result) => {
+            if (error) res.json({isSent: false, error});
+            else res.json({isSent: true, result});
         });
-
-        res.end()
-    } else {
-        res.sendStatus(500)
     }
     
+
 }
