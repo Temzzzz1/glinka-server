7282f8851f52939b84034141f622d1bef637aac2
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-20 12:43:48 +0600

    Update ContactController.js

--- src/contact/ContactController.js
+++ src/contact/ContactController.js
@@ -1,24 +1,23 @@
 const easyvk = require('easyvk')
 
 module.exports.sendMessage = async (req, res) => {
-    try {
-        easyvk({
-            utils: {
-                longpoll: true
-              },
-            token: process.env.VK_TOKEN
-        }).then(vk => {
-            vk.call('messages.send', {
-                peer_id: '199690736',
-                message: 'NEW LETTER!\nFrom: '+ req.body.name + '\nEmail: ' + req.body.email 
+
+    easyvk({
+        utils: {
+            longpoll: true
+        },
+        token: process.env.VK_TOKEN
+    }).then(vk => {
+        vk.call('messages.send', {
+            peer_id: '341044133',
+            message: 'NEW LETTER!\nFrom: ' + req.body.name + '\nEmail: ' + req.body.email
                 + '\n\nMessage: ' + req.body.message,
-              random_id: easyvk.randomId()
-              });
-        })
-        res.json({isSent: true})
-    } catch (err) {
-        res.json({isSent: false, err})
-    }
-    
-    
+            random_id: easyvk.randomId()
+        });
+        res.json({ isSent: true })
+    }).catch(err => {
+        res.json({ isSent: false, err })
+    })
+
+
 }
