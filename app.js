e692ecfa95a8646aa45936ffe9414da090f76cff
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-20 17:58:53 +0600

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 app.use(cookieParser())
-app.options('*', cors())
+app.use(cors())
 app.use(formData.parse());
 
 // Routes
