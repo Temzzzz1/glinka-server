d3af79fb2830089141f8aecf729cebbe68f985aa
Temzzzz1 <artem.rusanov.02@gmail.com>
2020-10-20 12:44:18 +0600

    add form-data

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ const mongoose = require('mongoose')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const formData = require('express-form-data');
 
 
 const PORT = process.env.PORT || 6080
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
 app.use(cookieParser())
 app.use(cors())
+app.use(formData.parse());
+
 
 
 
